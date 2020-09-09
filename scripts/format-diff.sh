9350948dec7ca743154b5d47772300efe5b97b57
Connor1996 <zbk602423539@gmail.com>
2020-09-09 23:15:41 +0800

    fix
    Signed-off-by: Connor1996 <zbk602423539@gmail.com>

--- scripts/format-diff.sh
+++ scripts/format-diff.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-git diff `git merge-base master HEAD` ./librocksdb_sys/crocksdb | clang-format-diff -style=google -p1 -i
+git diff `git merge-base master HEAD` ./librocksdb_sys/crocksdb | clang-format-diff-7 -style=google -p1 -i
